6c7aec9194df600e28017ee239becb3c0083d556
James Wagoner <jwagoner@compellon.com>
2016-09-16 14:09:43 -0700

    Updated selectize with new clear all plugin

---
+++ src/plugins/clear_all_filtered/plugin.js
@@ -0,0 +1,45 @@
+Selectize.define('clear_all_filtered', function (options) {
+	var self = this;
+
+	options = $.extend({
+		buttonContainerClass: 'selectize-clear-all-button-container',
+		clearAllButtonClass: 'selectize-clear-all-button',
+
+		html: function (data) {
+			return (
+				'<div class="' + data.buttonContainerClass + '">' +
+					'<button type="button" class="btn btn-default ' + data.clearAllButtonClass + '">Clear All</button>' +
+				'</div>'
+			);
+		}
+	}, options);
+
+	this.setup = (function () {
+		var original = self.setup;
+		return function () {
+			original.apply(this, arguments);
+			self.$clearAllContainer = $(options.html(options));
+			self.$dropdown.prepend(self.$clearAllContainer);
+
+			self.$clearAllContainer.on('click', '.' + options.clearAllButtonClass, function (e) {
+				e.preventDefault();
+				self.clear();
+				self.setTextboxValue('');
+				self.open();
+			});
+		};
+	})();
+
+	this.onBlur = (function () {
+		var original = self.onBlur;
+		return function (e, dest) {
+			if (!dest || dest === $('.' + options.clearAllButtonClass)[0]) {
+				if (e) {
+					e.preventDefault();
+				}
+				return;
+			}
+			return original.apply(this, arguments);
+		};
+	})();
+});
